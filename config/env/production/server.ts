96332724dcafb5c78b0db34e5c83c8004a10bb85
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-11-24 15:08:10 +0200

    /

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,12 +1,8 @@
 
 export default ({ env }) => ({
     proxy: true,
-    url: `${env('APP_URL')}/strapi`,
+    url: `${env('APP_URL')}`,
     app: {
         keys: env.array('APP_KEYS'),
-    },
-    admin: {
-        url: '/strapi/admin',
-        serveAdminPanel: true,
     },
 });
